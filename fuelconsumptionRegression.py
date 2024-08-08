e932682a71d3c10b03b636e7c88e3c6b55ae4e51
Marcos <126510019+MarcosHGF@users.noreply.github.com>
2024-08-08 17:42:37 -0300

    Update fuelconsumptionRegression.py

--- fuelconsumptionRegression.py
+++ fuelconsumptionRegression.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn import linear_model
 from sklearn.metrics import r2_score
 
+# DATA : https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv
 
 df = pd.read_csv("FuelConsumptionCo2.csv")
 
