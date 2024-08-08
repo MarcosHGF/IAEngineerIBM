e98fbf48a7957b1a34b37d499920a78d18cee1a3
Marcos <126510019+MarcosHGF@users.noreply.github.com>
2024-08-08 17:41:53 -0300

    Add files via upload

---
+++ fuelconsumptionRegression.py
@@ -0,0 +1,107 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+from sklearn.metrics import r2_score
+
+
+df = pd.read_csv("FuelConsumptionCo2.csv")
+
+# dataset visualization
+
+print(df.head())
+print(df.describe())
+
+# selecting and plotting
+
+cdf = df [['ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB', 'CO2EMISSIONS']]
+
+print(cdf.head(9))
+
+# Bar
+
+#cdf.hist()
+#plt.show()
+
+'''
+
+# Line emission x consumption
+
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("Emission")
+plt.show()
+
+# Line size x emission
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='green')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+# Line cylinders x emission
+
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color='red')
+plt.xlabel("Number of Cylinders")
+plt.ylabel("Emission")
+plt.show()
+
+'''
+
+# Selecting Data 80% - 20%
+
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+'''
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+'''
+# Modeling
+
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+
+# y = a(coefficient)x + b(intercept)
+
+print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+# Test
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y , test_y_) )
+
+
+# Fuel Consumption
+
+train_x = train[["FUELCONSUMPTION_COMB"]]
+test_x = test[["FUELCONSUMPTION_COMB"]]
+regr.fit(train_x, train_y)
+
+predictions = regr.predict(test_x)
+
+print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
+print("Mean Absolute Error: %.2f" % np.mean(np.absolute(predictions - test_y)))
+
+plt.scatter(train.FUELCONSUMPTION_COMB, train.CO2EMISSIONS,  color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
